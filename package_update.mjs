ddeedd22a246763b1e97fa00a0bacee5278a2895
coolguy284 <coolguy284v64@gmail.com>
2026-05-10 01:44:59 -0400

    start reworking package update

--- package_update.mjs
+++ package_update.mjs
@@ -8,32 +8,39 @@ import {
 import { get } from 'node:https';
 
 const FILES_TO_CHECK = ['package.json', 'package-basic.json'];
+const DEPS_TO_CHECK = ['dependencies', 'optionalDependencies', 'devDependencies'];
 
 async function processPackages(projectFolders) {
   // Get all files
   
-  let files = {};
+  let files = new Map();
   
   for (const folder of projectFolders) {
     for (const file of FILES_TO_CHECK) {
       const path = folder + '/' + file;
+      
+      console.log(`Processing file ${path}`);
+      
       if (await access(path)) {
         const fileText = (await readFile(path)).toString();
         const fileJson = JSON.parse(fileText);
-        if ('dependencies' in fileJson) {
-          console.log(`Processing file ${path}`);
-          const fileLines = fileText.split(/\r?\n/);
-          let fileBlankLines = [];
-          for (let i = 0; i < fileLines.length; i++) {
-            if (/^ +$/.test(fileLines[i])) fileBlankLines.push(i);
-          }
-          files[path] = {
+        const fileLines = fileText.split(/\r?\n/);
+        
+        let fileBlankLines = [];
+          
+        for (let i = 0; i < fileLines.length; i++) {
+          if (/^ +$/.test(fileLines[i])) fileBlankLines.push(i);
+        }
+        
+        files.set(
+          path,
+          {
             text: fileText,
             json: fileJson,
             line: fileLines,
             blankLines: fileBlankLines,
-          };
-        }
+          },
+        );
       }
     }
   }
@@ -42,47 +49,62 @@ async function processPackages(projectFolders) {
   
   let packages = new Set();
   
-  for (const file of Object.values(files)) {
-    Object.keys(file.json.dependencies).forEach(x => packages.add(x));
+  for (const { json } of files.values()) {
+    for (const { dependencyPath } of DEPS_TO_CHECK) {
+      Object.keys(json[dependencyPath]).forEach(x => packages.add(x));
+    }
   }
   
   // Get package info
   
-  let packageVersion = {};
+  let packageVersion = new Map();
   
   for (const packageName of packages) {
     console.log(`Getting package ${packageName}`);
-    packageVersion[packageName] = await new Promise(r => {
-      get(`https://registry.npmjs.org/${packageName}`, {
-        headers: {
-          'accept': 'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*',
-        },
-      }, res => {
-        let chunks = [];
-        res.on('data', c => chunks.push(c));
-        res.on('end', () => r(JSON.parse(Buffer.concat(chunks).toString())['dist-tags'].latest));
-      });
-    });
+    
+    packageVersion.set(
+      packageName,
+      await new Promise(r => {
+        get(`https://registry.npmjs.org/${packageName}`, {
+          headers: {
+            'accept': 'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*',
+          },
+        }, res => {
+          let chunks = [];
+          res.on('data', c => chunks.push(c));
+          res.on('end', () => r(JSON.parse(Buffer.concat(chunks).toString())['dist-tags'].latest));
+        });
+      }),
+    );
   }
   
   // Replace version numbers
   
-  for (const file of Object.values(files)) {
-    for (const packageName in file.json.dependencies) {
-      file.json.dependencies[packageName] = '^' + packageVersion[packageName];
+  for (const file of files.values()) {
+    const { json, blankLines } = file;
+    
+    for (const packageName in json.dependencies) {
+      json.dependencies[packageName] = '^' + packageVersion[packageName];
     }
-    file.lines = JSON.stringify(file.json, null, 2).split('\n');
-    for (const line of file.blankLines) {
-      file.lines.splice(line, 0, '    ');
+    
+    const newLines =
+      JSON.stringify(file.json, null, 2)
+      .split('\n');
+    
+    for (const line of blankLines) {
+      newLines.splice(line, 0, '    ');
     }
-    file.text = file.lines.join('\n') + '\n';
+    
+    file.newText =
+      newLines.join('\n') +
+      '\n';
   }
   
   // Replace files
   
-  for (const path in files) {
+  for (const [ path, newText ] in files) {
     console.log(`Updating file ${path}`);
-    await writeFile(path, files[path].text);
+    await writeFile(path, newText);
   }
 }
 
