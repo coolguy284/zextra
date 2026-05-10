137985dcfce5f618018f482c24f354cf96287cbc
coolguy284 <coolguy284v64@gmail.com>
2026-05-10 00:40:47 -0400

    improve package update code quality

--- package_update.mjs
+++ package_update.mjs
@@ -7,26 +7,26 @@ import {
 } from 'node:fs/promises';
 import { get } from 'node:https';
 
-const filesToCheck = ['package.json', 'package-basic.json'];
+const FILES_TO_CHECK = ['package.json', 'package-basic.json'];
 
 async function processPackages(projectFolders) {
-  var file, packageName, path;
+  // Get all files
   
-  // get all files
-  var files = {};
+  let files = {};
   
-  for (var folder of projectFolders) {
-    for (file of filesToCheck) {
-      path = folder + '/' + file;
+  for (const folder of projectFolders) {
+    for (const file of FILES_TO_CHECK) {
+      const path = folder + '/' + file;
       if (await access(path)) {
-        var fileText = (await readFile(path)).toString();
-        var fileJson = JSON.parse(fileText);
+        const fileText = (await readFile(path)).toString();
+        const fileJson = JSON.parse(fileText);
         if ('dependencies' in fileJson) {
           console.log(`Processing file ${path}`);
-          var fileLines = fileText.split(/\r?\n/);
-          var fileBlankLines = [];
-          for (var i = 0; i < fileLines.length; i++)
+          const fileLines = fileText.split(/\r?\n/);
+          let fileBlankLines = [];
+          for (let i = 0; i < fileLines.length; i++) {
             if (/^ +$/.test(fileLines[i])) fileBlankLines.push(i);
+          }
           files[path] = {
             text: fileText,
             json: fileJson,
@@ -38,16 +38,19 @@ async function processPackages(projectFolders) {
     }
   }
   
-  // get packages
-  var packages = new Set();
+  // Get packages
   
-  for (file of Object.values(files))
+  let packages = new Set();
+  
+  for (const file of Object.values(files)) {
     Object.keys(file.json.dependencies).forEach(x => packages.add(x));
+  }
   
-  // get package info
-  var packageVersion = {};
+  // Get package info
   
-  for (packageName of packages) {
+  let packageVersion = {};
+  
+  for (const packageName of packages) {
     console.log(`Getting package ${packageName}`);
     packageVersion[packageName] = await new Promise(r => {
       get(`https://registry.npmjs.org/${packageName}`, {
@@ -55,25 +58,29 @@ async function processPackages(projectFolders) {
           'accept': 'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*',
         },
       }, res => {
-        var chunks = [];
+        let chunks = [];
         res.on('data', c => chunks.push(c));
         res.on('end', () => r(JSON.parse(Buffer.concat(chunks).toString())['dist-tags'].latest));
       });
     });
   }
   
-  // replace version numbers
-  for (file of Object.values(files)) {
-    for (packageName in file.json.dependencies)
+  // Replace version numbers
+  
+  for (const file of Object.values(files)) {
+    for (const packageName in file.json.dependencies) {
       file.json.dependencies[packageName] = '^' + packageVersion[packageName];
+    }
     file.lines = JSON.stringify(file.json, null, 2).split('\n');
-    for (var line of file.blankLines)
+    for (const line of file.blankLines) {
       file.lines.splice(line, 0, '    ');
+    }
     file.text = file.lines.join('\n') + '\n';
   }
   
-  // replace files
-  for (path in files) {
+  // Replace files
+  
+  for (const path in files) {
     console.log(`Updating file ${path}`);
     await writeFile(path, files[path].text);
   }
